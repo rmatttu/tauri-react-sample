947c34fa9765e7c8ee03dc4aa3d5cff31e25274a
mggggk <mggggk@gmail.com>
2022-06-22 18:33:18 +0900

    Add showing local image file

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 import { invoke } from '@tauri-apps/api/tauri'
 
+import { appDir, join } from '@tauri-apps/api/path';
+import { convertFileSrc } from '@tauri-apps/api/tauri';
+import { open } from '@tauri-apps/api/dialog'
+import { readBinaryFile, BaseDirectory } from '@tauri-apps/api/fs';
+
+
 function App() {
+
+  function _arrayBufferToBase64( buffer:Uint8Array ) {
+      var binary = '';
+      var bytes = new Uint8Array( buffer );
+      var len = bytes.byteLength;
+      for (var i = 0; i < len; i++) {
+          binary += String.fromCharCode( bytes[ i ] );
+      }
+      return window.btoa( binary );
+  }
+
   function executeCommands() {
     invoke('simple_command')
+  }
+
+  function testLocalResource() {
+    const assetUrl = convertFileSrc("/Users/hawk/Desktop/DSC_4395.NEF.jpg");
+    const image = document.getElementById("image-view",)! as HTMLImageElement;
+    const source = document.createElement('source');
+
+    open().then(files => {
+      console.log(files)
+      if(typeof files == 'string'){
+        readBinaryFile(files).then(a => {
+          console.log(a.byteLength)
+          const data = _arrayBufferToBase64(a)
+          image.src="data:image/jpg;base64," + data
+        })
+      }
+    })
   }
 
   function executeCommandWithMessage() {
@@ -39,6 +73,10 @@ function App() {
           <input id="input1"></input>
           <button onClick={executeCommandWithMessage}>Click to execute command</button>
           <textarea id="textarea1"></textarea>
+        </div>
+        <div>
+          <button onClick={testLocalResource}>Click to execute command</button>
+          <img id="image-view" src="" alt="" width="400" height="400"></img>
         </div>
       </header>
     </div>
